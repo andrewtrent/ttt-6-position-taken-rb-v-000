0bce44d08ffb0d35d846c02c95146c0df8111863
Andrew Trent <andrewtrentdesign@gmail.com>
2019-12-29 06:42:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,15 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(b, i)
-#  if b[i] == nil
-#    false
-#  elsif b[i].strip == ""
-#    false
+
  if b[i] == "X" || b[i] == "O"
-    true
- #elsif b[i] == "O"
-#   true
-else
+
+ else
    false
   end
 end
